ee94a6c9691fd93ec9cbfb944abeecc00b6e0c99
George W. Walker <george@georgewwalker.com>
2019-09-08 20:06:13 -0500

    Dynamically get repos from GitHub

--- src/views/Software.tsx
+++ src/views/Software.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Section from "../components/Section";
+import Octokit, { ReposGetResponse } from "@octokit/rest";
+
+const excludedRepos = ["gallery-part-1-pkmnct", "625-hw4", "pkmnct.github.io"];
 
 const Software = () => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    if (repos.length) {
+      const filtered = repos.filter(
+        (repo: ReposGetResponse) => !excludedRepos.includes(repo.name)
+      );
+      filtered.sort((a: ReposGetResponse, b: ReposGetResponse) =>
+        (a.stargazers_count + a.watchers_count) / 2 >
+        (b.stargazers_count + b.watchers_count) / 2
+          ? -1
+          : (a.stargazers_count + a.watchers_count) / 2 <
+            (b.stargazers_count + b.watchers_count) / 2
+          ? 1
+          : 0
+      );
+      console.log(filtered);
+    } else {
+      const octokit = new Octokit();
+      octokit.repos
+        .listForUser({ username: "pkmnct", type: "all", sort: "updated" })
+        .then(({ data }) => setRepos(data));
+    }
+  }, [repos]);
+
   return (
     <Section className="Software">
       <h1>Software Portfolio goes here</h1>
