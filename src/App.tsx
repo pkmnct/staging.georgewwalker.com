ac02758ee3eac82ff018c4afc28c4789559afe2a
George W. Walker <george@georgewwalker.com>
2019-09-09 19:11:35 -0500

    Move header logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,78 +3,13 @@ import Software from "./views/Software";
 import Photo from "./views/Photo";
 import Tech from "./views/Tech";
 import Web from "./views/Web";
+import React from "react";
 import Header from "./components/Header";
-import React, { useEffect, useState } from "react";
-import NavigationLink from "./components/NavigationLink";
-import Navigation from "./components/Navigation";
-import BackgroundHolder from "./components/Backgrounds/Holder";
-import ParticlesBackground from "./components/Backgrounds/Particles";
-import GradientBackground from "./components/Backgrounds/Gradient";
-import Slideshow from "./components/Slideshow";
 
 const App = (props: RouteComponentProps) => {
-  // Hover handlers
-  const [photoHover, setPhotoHover] = useState(false);
-  const [webHover, setWebHover] = useState(false);
-  const handleWebMouseEnter = () => setWebHover(true);
-  const handleWebMouseLeave = () =>
-    props.location.pathname !== "/web" && setWebHover(false);
-  const handlePhotoMouseEnter = () => setPhotoHover(true);
-  const handlePhotoMouseLeave = () =>
-    props.location.pathname !== "/photo" && setPhotoHover(false);
-
-  // Keep backgrounds for each section
-  useEffect(() => {
-    setWebHover(false);
-    setPhotoHover(false);
-    switch (props.location.pathname) {
-      case "/web":
-        setWebHover(true);
-        break;
-      case "/photo":
-        setPhotoHover(true);
-        break;
-    }
-    if (props.location.pathname === "/web") {
-      setWebHover(true);
-    } else {
-      setWebHover(false);
-    }
-  }, [props.location.pathname]);
-
   return (
     <>
-      <GradientBackground showing={!photoHover || webHover} />
-      <ParticlesBackground showing={webHover} />
-      <BackgroundHolder showing={photoHover && !webHover}>
-        <Slideshow
-          isPlaying={photoHover}
-          images={[
-            "/images/05.03.15.BSFSLightning.01.jpg",
-            "/images/03.08.15.Birds.GW.01.jpg"
-          ]}
-        />
-      </BackgroundHolder>
-      <Header>
-        <Navigation>
-          <NavigationLink to="/software">Software Engineer</NavigationLink>
-          <NavigationLink
-            to="/web"
-            onMouseEnter={handleWebMouseEnter}
-            onMouseLeave={handleWebMouseLeave}
-          >
-            Web Developer
-          </NavigationLink>
-          <NavigationLink
-            to="/photo"
-            onMouseEnter={handlePhotoMouseEnter}
-            onMouseLeave={handlePhotoMouseLeave}
-          >
-            Photographer
-          </NavigationLink>
-          <NavigationLink to="/tech">Tech Enthusiast</NavigationLink>
-        </Navigation>
-      </Header>
+      <Header />
       <Route exact path="/Software" component={Software} />
       <Route exact path="/Tech" component={Tech} />
       <Route exact path="/Photo" component={Photo} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
-import React, { HTMLProps } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, RouteComponentProps, withRouter } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "./Logo";
+import NavigationLink from "../components/NavigationLink";
+import Navigation from "../components/Navigation";
+import BackgroundHolder from "../components/Backgrounds/Holder";
+import ParticlesBackground from "../components/Backgrounds/Particles";
+import GradientBackground from "../components/Backgrounds/Gradient";
+import Slideshow from "../components/Slideshow";
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -12,15 +18,74 @@ const StyledHeader = styled.header`
   text-align: center;
 `;
 
-const Header = (props: HTMLProps<HTMLElement>) => {
+const Header = (props: RouteComponentProps) => {
+  // Hover handlers
+  const [photoHover, setPhotoHover] = useState(false);
+  const [webHover, setWebHover] = useState(false);
+  const handleWebMouseEnter = () => setWebHover(true);
+  const handleWebMouseLeave = () =>
+    props.location.pathname !== "/web" && setWebHover(false);
+  const handlePhotoMouseEnter = () => setPhotoHover(true);
+  const handlePhotoMouseLeave = () =>
+    props.location.pathname !== "/photo" && setPhotoHover(false);
+
+  // Keep backgrounds for each section
+  useEffect(() => {
+    setWebHover(false);
+    setPhotoHover(false);
+    switch (props.location.pathname) {
+      case "/web":
+        setWebHover(true);
+        break;
+      case "/photo":
+        setPhotoHover(true);
+        break;
+    }
+    if (props.location.pathname === "/web") {
+      setWebHover(true);
+    } else {
+      setWebHover(false);
+    }
+  }, [props.location.pathname]);
+
   return (
-    <StyledHeader>
-      <StyledLink to="/">
-        <Logo />
-      </StyledLink>
-      {props.children}
-    </StyledHeader>
+    <>
+      <GradientBackground showing={!photoHover || webHover} />
+      <ParticlesBackground showing={webHover} />
+      <BackgroundHolder showing={photoHover && !webHover}>
+        <Slideshow
+          isPlaying={photoHover}
+          images={[
+            "/images/05.03.15.BSFSLightning.01.jpg",
+            "/images/03.08.15.Birds.GW.01.jpg"
+          ]}
+        />
+      </BackgroundHolder>
+      <StyledHeader>
+        <StyledLink to="/">
+          <Logo />
+        </StyledLink>
+        <Navigation>
+          <NavigationLink to="/software">Software Engineer</NavigationLink>
+          <NavigationLink
+            to="/web"
+            onMouseEnter={handleWebMouseEnter}
+            onMouseLeave={handleWebMouseLeave}
+          >
+            Web Developer
+          </NavigationLink>
+          <NavigationLink
+            to="/photo"
+            onMouseEnter={handlePhotoMouseEnter}
+            onMouseLeave={handlePhotoMouseLeave}
+          >
+            Photographer
+          </NavigationLink>
+          <NavigationLink to="/tech">Tech Enthusiast</NavigationLink>
+        </Navigation>
+      </StyledHeader>
+    </>
   );
 };
 
-export default Header;
+export default withRouter(Header);
